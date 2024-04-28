4262a2c19147624e7e36184e397d9d668c1d2fb7
Manuel Guzman <manuelguzmanvilches@gmail.com>
2024-04-28 18:13:05 -0400

    [ADD] primer commit

---
+++ src/main/java/com/mg/registroestudiantes/registroestudiantes/RegistroEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.mg.registroestudiantes.registroestudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroEstudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/registroestudiantes/registroestudiantes/RegistroEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.registroestudiantes.registroestudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
